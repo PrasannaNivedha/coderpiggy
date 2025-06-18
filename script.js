97bb364c882762dcc2207f8a862768ffbc493811
PrasannaNivedha <nivedhaprasannaindia@gmail.com>
2025-06-18 18:45:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,131 @@
+//projects frames
+let pages = [];
+pages[0] = document.getElementById("iframe1");
+pages[1] = document.getElementById("iframe2");
+pages[2] = document.getElementById("iframe3");
 
-//for modal display only
-let contacky = document.getElementById('contactform')
+//initial page source
+document.getElementById('goto').href = pages[0].src ;
 
-contacky.addEventListener("submit", funk);
-
-function funk(event)
+//to load initial frame
+for (let i=1; i<pages.length; i++)
 {
-    event.preventDefault();
+   pages[i].style.display = "none";
+}
 
-    if(contacky.checkValidity())
+//function to change as per button click
+document.getElementById('buttons').addEventListener('click',changerfunk);
+let j=1;
+
+function changerfunk(event)
+{
+    if(event.target.innerHTML === "Next")
     {
-        let modal = new bootstrap.Modal(document.getElementById('myModal'));
-        modal.show();
-        contacky.reset();
-
+      if(j<pages.length)
+        {
+          for(let k=0; k<pages.length; k++)
+          {
+            if(k==j)
+            {pages[k].style.display = "block";
+                document.getElementById('goto').href = pages[k].src ;
+            }
+            else
+            {pages[k].style.display = "none";}
+          }
+          j++;
+        }
+      else
+        {
+          j = 0;
+          for(k=0; k<pages.length; k++)
+          {
+            if(k==j)
+            {pages[k].style.display = "block";
+                document.getElementById('goto').href = pages[k].src ;
+            }
+            else
+            {pages[k].style.display = "none";}
+          }
+          j++;
+        }
     }
     else
     {
-        contacky.reportValidity();
+        if(j>=0)
+        {
+          for(let k=0; k<pages.length; k++)
+          {
+            if(k==j)
+            {pages[k].style.display = "block";
+                document.getElementById('goto').href = pages[k].src ;
+            }
+            else
+            {pages[k].style.display = "none";}
+          }
+          j--;
+        }
+      else
+        {
+          j = pages.length-1;
+          for(k=0; k<pages.length; k++)
+          {
+            if(k==j)
+            {pages[k].style.display = "block";
+                document.getElementById('goto').href = pages[k].src ;
+            }
+            else
+            {pages[k].style.display = "none";}
+          }
+          j--;
+        }
+    
     }
-
+    
 }
 
-//collapsing navbar quickly 
-document.addEventListener("DOMContentLoaded", function () {
-    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
-    const navbarCollapse = document.querySelector(".navbar-collapse");
+let slideshowinterval = setInterval(timefunk, 5000)
 
-    navLinks.forEach(function (link) {
-      link.addEventListener("click", function () {
-        if (navbarCollapse.classList.contains("show")) {
-          new bootstrap.Collapse(navbarCollapse).hide();
+function timefunk()
+{
+    if(j<pages.length)
+        {
+          for(let k=0; k<pages.length; k++)
+          {
+            if(k==j)
+            {pages[k].style.display = "block";
+              document.getElementById('goto').href = pages[k].src ;
+            }
+            else
+            {pages[k].style.display = "none";}
+          }
+          j++;
         }
-      });
-    });
-  });
+      else
+        {
+          j = 0;
+          for(k=0; k<pages.length; k++)
+          {
+            if(k==j)
+            {pages[k].style.display = "block";
+                document.getElementById('goto').href = pages[k].src ;
+            }
+            else
+            {pages[k].style.display = "none";}
+          }
+          j++;
+        }
+}
+
+document.getElementById('pageview').addEventListener('mouseover',pausefunction)
+
+function pausefunction()
+{
+    clearInterval(slideshowinterval);
+
+}
+document.getElementById('pageview').addEventListener('mouseout',resumefunk);
+
+function resumefunk()
+{
+    slideshowinterval = setInterval(timefunk, 5000);
+}
